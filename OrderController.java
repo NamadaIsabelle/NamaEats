3b833f7dc3e0b46c01dbe1b2e82f7c7b75853269
Namada <202796574+NamadaIsabelle@users.noreply.github.com>
2026-02-28 06:13:20 +0300

    Add files via upload

---
+++ OrderController.java
@@ -0,0 +1,34 @@
+package com.namaeats.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.namaeats.model.Order;
+import com.namaeats.repository.OrderRepository;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderRepository orderRepository;
+
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @GetMapping
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    @PostMapping
+    public Order placeOrder(@RequestBody Order order) {
+        order.setOrderTime(java.time.LocalDateTime.now());
+        return orderRepository.save(order);
+    }
+}
---
+++ UserController.java
@@ -0,0 +1,28 @@
+package com.namaeats.controller;
+
+import com.namaeats.model.User;
+import com.namaeats.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+}
