efed5cc91832a19f892324ca6ecc12630a1372e6
Namada <202796574+NamadaIsabelle@users.noreply.github.com>
2026-02-28 05:03:05 +0300

    Add files via upload

---
+++ NamaEatsApplication.java
@@ -0,0 +1,13 @@
+package com.namaeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamaEatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamaEatsApplication.class, args);
+	}
+
+}
